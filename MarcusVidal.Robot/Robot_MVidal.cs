02b436b51e280224c4a368a3d003e4447b224050
mvlvidal <marcusvlvidal@gmail.com>
2019-06-27 01:26:07 -0300

    Finalizado

--- MarcusVidal.Robot/Robot_MVidal.cs
+++ MarcusVidal.Robot/Robot_MVidal.cs
@@ -8,14 +8,19 @@ namespace MarcusVidal.Robo
     public class Robot_MVidal : AdvancedRobot
     {
 
+        /* 
+            Robô criado por Marcus Vidal: https://github.com/mvlvidal/Robot_MarcusVidal.git
+        */
+
         public override void Run()
         {
-            SetColors(Color.Black, Color.Black, Color.DarkOrange);
+            //Seta as cpres do Robô: corpo, arma, radar, bala e faixa do radar
+            SetColors(Color.Black, Color.Yellow, Color.Yellow, Color.OrangeRed, Color.White);
                   
 
             while (true)
             {                           
-                
+                //Movimentação repetitiva do robô em uma forma parecida com um oito.
                 for (int i = 0; i < 5; i++)
                 {
                     Ahead(100);
@@ -23,6 +28,7 @@ namespace MarcusVidal.Robo
                     Scan();
 
                 }
+
                 for (int i = 0; i < 5; i++)
                 {
                     Ahead(100);
@@ -30,6 +36,7 @@ namespace MarcusVidal.Robo
                     Scan();
                 }
 
+                
 
             }
             
@@ -37,57 +44,86 @@ namespace MarcusVidal.Robo
 
         public override void OnHitWall(HitWallEvent evento)
         {
-            TurnRight(50);
+            //Movimentação do robô caso haja colizão com a parede
             Back(150);
+            TurnRight(50);
+            
         }
 
-        public override void OnBulletHit(BulletHitEvent evnt)
+        public override void OnHitByBullet(HitByBulletEvent evento)
         {
+            Console.WriteLine($"{Name} foi alvejado por {evento.Name} com {evento.Power} de dano!!");
+
+            //Movimentação de defesa ao receber um tiro
             Back(100);
+
             TurnLeft(45);
-            Ahead(100);
+
             Scan();
+        }
+
+        public override void OnBulletHit(BulletHitEvent evento)
+        {
+            Console.WriteLine($"{Name} baleou {evento.VictimName}!!");
         }
 
         public override void OnScannedRobot(ScannedRobotEvent evento)
         {
+            //Colhe o angulo do inimigo scaneado apartir da posição do robô
             double anguloInimigo = Utils.NormalRelativeAngleDegrees(evento.Bearing + Heading - GunHeading);
 
-            
+            //Gira a rma do robô para a direção do inimigo scaneado utilização o seu angulo relativo colhido
             TurnGunRight(anguloInimigo);
+            Stop();
 
-            Fire(setaInsensidade(Energy));           
-            
-            Scan();
+            //Atira no inigimo de acordo com a energia do robô
+            Fire(setaInsensidade(Energy));
+
+            Ahead(5);
         }
 
         public override void OnHitRobot(HitRobotEvent evento)
         {
+
+            Console.WriteLine($"{Name} colidiu com {evento.Name}!!");
+
+            //Colhe o angulo do inimigo scaneado apartir da posição do robô que colidiu
             double anguloInimigo = Utils.NormalRelativeAngleDegrees(evento.Bearing + Heading - GunHeading);
 
+            Back(100);
+
+            Stop();
+
+            //Gira a rma do robô para a direção do inimigo da colisão utilizando o seu angulo relativo colhido
             TurnGunRight(anguloInimigo);
 
-            Fire(3);
+            //Atira no inigimo de acordo com a energia do robô
+            FireBullet(setaInsensidade(Energy));
 
         }
 
         public int setaInsensidade(double energia) {
 
-            //TODO fazer comparações para retornar a intensidade do tiro
+            //Metodo que define a intensidade do tiro de acordo com a temperatura da arma e a energia do robô
 
             int resultado = 0;
 
-            if (energia < 3)
+            if (GunCoolingRate == 0 && energia > 70)
             {
                 resultado = 3;
             }
-            else {
+            else if (GunCoolingRate == 0 && energia > 30 && energia < 70 )
+            {
+                resultado = 2;
+            }
+            else
+            {
                 resultado = 1;
             }
 
             return resultado;
         }
 
-  
     }
+
 }
